aef9744c6118dea12f2dfbef392db0cd2597a4cf
Jhon <faheemp503@gmail.com>
2023-05-14 14:21:31 +0530

    by Faheem v2

--- assignment/project/app.js
+++ assignment/project/app.js
@@ -1,29 +1,35 @@
 import express from 'express'
-import {join} from 'path'
-const app=express()
+import { join } from 'path'
+const app = express()
 
-const port=3000;
+const port = 3000;
 
 app.use(express.static('views'))
+app.use(express.static('videos'))
+
 
 // app.get('/home',(req,res)=>{
 //     res.send('<h1>Home</h1>')
 // })
 
-app.get('/home',(req,res)=>{
+app.get('/home', (req, res) => {
     // res.sendFile(process.cwd(),'/views/index.html') 
-    res.sendFile(join(process.cwd(),'/views/index.html'))
+    res.sendFile(join(process.cwd(), '/views/index.html'))
 
 })
 
-app.get('/filedownload',(req,res)=>{
-    res.download(join(process.cwd(),'/public/images/download.jpeg'))
+app.get('/filedownload', (req, res) => {
+    res.download(join(process.cwd(), '/public/videos/be.mp4'))
+})
+
+app.get('/imgdownload', (req, res) => {
+    res.download(join(process.cwd(), '/public/images/download.jpeg'))
 })
 
 
 
 
 
-app.listen(port,()=>{
-    console.log('server is running'+port)
+app.listen(port, () => {
+    console.log('server is running' + port)
 })
